21783b04040e7e5fe28587f00506eb5b16705668
pijnr <pauloivanju@gmail.com>
2017-03-20 23:47:31 -0300

    Exercicio02.java
    Multiplicação

---
+++ Exerccio02.java/src/exerccio02/java/Exerccio02Java.java
@@ -0,0 +1,14 @@
+
+package exerccio02.java;
+
+
+public class Exerccio02Java {
+
+
+    public static void main(String[] args) {
+     Integer x = 2, y = 4 ;
+        Integer Mult = x*y;
+        System.out.println("A Soma é: " + Mult);
+    }
+    
+}
